7bdaa6984164049ced98cbcdc9838cb752b174b5
TenOne506 <w13852770506@163.com>
2024-12-18 19:38:38 +0800

    不会了，先去看rust圣经了

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -26,6 +26,9 @@ impl ParsePosNonzeroError {
 
     // TODO: Add another error conversion function here.
     // fn from_parse_int(???) -> Self { ??? }
+    fn from_parse_int(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -43,9 +46,14 @@ impl PositiveNonzeroInteger {
     fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
         // TODO: change this to return an appropriate error instead of panicking
         // when `parse()` returns an error.
-        let x: i64 = s.parse().unwrap();
+        // let x: i64 = s.parse().unwrap();
+        // Self::new(x).map_err(ParsePosNonzeroError::from_creation)
+              // 使用 `map_err` 处理 `s.parse()` 可能返回的错误
+        let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parse_int)?;
+              // 使用 `map_err` 处理 `Self::new(x)` 可能返回的错误
         Self::new(x).map_err(ParsePosNonzeroError::from_creation)
     }
+
 }
 
 fn main() {
